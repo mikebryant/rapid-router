93ae40f57ee99b87a526592ec7b682ea57ffa731
Mike Bryant <mike@mikebryant.me.uk>
2015-11-22 23:29:59 +0000

    Switch to better selenium setup

--- test_settings.py
+++ test_settings.py
@@ -1,3 +1,5 @@
+from selenium import webdriver
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -7,6 +9,14 @@ INSTALLED_APPS = ['game']
 PIPELINE_ENABLED = False
 ROOT_URLCONF = 'django_autoconfig.autourlconf'
 STATIC_ROOT='.tests_static/'
+
+SELENIUM_WEBDRIVERS = {
+    'default': {
+        'callable': webdriver.Firefox,
+        'args': (),
+        'kwargs': {},
+    }
+}
 
 from django_autoconfig.autoconfig import configure_settings
 configure_settings(globals())
