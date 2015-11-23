86ad677057961ddc4b9bd8241d32dbddc7606fa6
Mike Bryant <m@ocado.com>
2015-11-23 16:33:29 +0000

    Trying selenium remote webdriver stuff with saucelabs

--- test_settings.py
+++ test_settings.py
@@ -1,4 +1,36 @@
+import os
 from selenium import webdriver
+
+SAUCE_URL = "https://%s:%s@ondemand.saucelabs.com/wd/hub" % (
+    os.environ.get('SAUCE_USERNAME', ''),
+    os.environ.get('SAUCE_ACCESS_KEY', ''),
+)
+
+
+def generate_selenium_capabilities(base_name):
+    caps = getattr(webdriver.DesiredCapabilities, base_name).copy()
+    caps['build'] = caps['tunnel-identifier'] = os.environ.get('TRAVIS_JOB_NUMBER', '')
+    return caps
+
+SELENIUM_WEBDRIVERS = {
+    'default': {
+        'callable': webdriver.Firefox,
+        'args': (),
+        'kwargs': {},
+    },
+    'remote': {
+        'callable': webdriver.Remote,
+        'args': (),
+        'kwargs': {
+            'desired_capabilities': generate_selenium_capabilities(
+                os.environ.get(
+                    'SELENIUM_REMOTE_DRIVER',
+                    'FIREFOX',
+                ),
+            ),
+        },
+    },
+}
 
 DATABASES = {
     'default': {
@@ -8,15 +40,7 @@ DATABASES = {
 INSTALLED_APPS = ['game']
 PIPELINE_ENABLED = False
 ROOT_URLCONF = 'django_autoconfig.autourlconf'
-STATIC_ROOT='.tests_static/'
-
-SELENIUM_WEBDRIVERS = {
-    'default': {
-        'callable': webdriver.Firefox,
-        'args': (),
-        'kwargs': {},
-    }
-}
+STATIC_ROOT = '.tests_static/'
 
 from django_autoconfig.autoconfig import configure_settings
 configure_settings(globals())
